dc71378aefcf42fe2c03f670c0f882f4010a5388
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2023-05-29 04:29:37 +0400

    feat: адаптивность и кроссбраузерность

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -44,18 +44,6 @@ export default class FormValidator {
     } else {
       this._hideInputError(inputElement);
     }
-
-    console.log(inputElement.validity.valid);
-    /*
-    var radios = document.getElementsByName("yesno");
-    var formValid = false;
-    var i = 0;
-    while (!formValid && i < radios.length) {
-        if (radios[i].checked) formValid = true;
-        i++;
-    }
-    if (!formValid) alert("Must check some option!");
-    return formValid;*/
   }
 
   toggleButtonState() {
